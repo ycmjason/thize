b3d946b457cba95542a9c09d6937ab57051ab5b9
Jason Yu <me@ycmjason.com>
2018-04-30 22:19:30 +0100

    added script to publish to aliases

---
+++ scripts/publishAll.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const package = require('../package.json');
+const aliases = package.aliases;
+
+const { execSync } = require('child_process');
+const { writeFileSync } = require('fs');
+const { join } = require('path');
+
+const myExecSync = (cmd) => execSync(cmd, {
+  cwd: join(__dirname, '..'),
+  stdio: 'inherit',
+});
+
+const writePackage = (package) => writeFileSync(JSON.stringify(package));
+
+aliases.forEach(a => {
+  writePackage({
+    ...package,
+    name: a,
+  });
+
+  myExecSync('npm publish');
+});
+
+myEexecSync('git checkout package.json');
